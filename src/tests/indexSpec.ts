430ffef759483d4be0384ab0b5c278be8c0d3648
April <aprilnicole013@gmail.com>
2021-10-06 23:57:56 -0400

    Add supertesting

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,6 +1,18 @@
 //holds tests for code in index.js file
 import myFunc from '../index';
+import supertest from 'supertest';
+import app from '../index';
+
+const request = supertest(app);
+describe('Test endpoint responses', () => {
+    it('gets the api endpoint', async (done) => {
+        const response = await request.get('/api');
+        expect(response.status).toBe(200);
+        done();
+    }
+)});
 
 it('expect myFunc(5) to equal 25', () => {
     expect(myFunc(5)).toEqual(25)
 })
+
