290502580d1f531fc44be007610b88a78477c924
April <aprilnicole013@gmail.com>
2021-10-14 22:29:49 -0400

    Add image request validator

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import images from "./routes/images";
+import images from './routes/images';
+import imageValidator from './middleware/imageValidator'
 import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.use('/api', );
+app.use('/api/images', imageValidator, images);
 
 app.listen(port, () => {
     console.log(`server started at localhost:${port}`);
---
+++ src/middleware/imageValidator.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const imageValidator = () => {}
+
+export default imageValidator
