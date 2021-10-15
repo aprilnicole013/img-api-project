7daa1598ee2a2e88681aeefe7adf766ca20286d5
April <aprilnicole013@gmail.com>
2021-10-14 23:09:43 -0400

    Configure images routes and operations

--- src/operations/resizeImage.ts
+++ src/operations/resizeImage.ts
@@ -4,6 +4,23 @@ import { error } from 'console';
 
 const sharp = require('sharp')
 
-const resizeImage = async () => {}
+const resizeImage = async (
+    width: number,
+    height: number,
+    filename: string
+): Promise<{ success: boolean; result: string }> => {
+    try{
+        const newFilename = `${filename}_${width}_${height}.jpg`;
+
+        if (!fs.existsSync(path.join('./assets', 'thumbs', newFilename))) {
+            console.log('creating resized images');
+            await sharp(`./assets/full/${filename}.jpg`).
+            resizeImage(width, height).toFile(path.join('./assets', 'thumbnails', newFilename))
+        }
+        return { success: true, result: newFilename }
+    }catch (err){
+        return {success: false, result: "Cannot resize image"}
+    }
+}
 
 export default resizeImage
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -4,6 +4,19 @@ import path from 'path';
 
 const images = express.Router();
 
-images.get('', async (req: express.Request, res: express.Response) => {})
+images.get('/', async (req: express.Request, res: express.Response) => {
+    const width = parseInt(req.query.width as string);
+    const height = parseInt(req.query.height as string);
+
+    if (isNaN(width) != true && isNaN(height) != true){
+        const filename = req.query.filename as string;
+        const resize = await resizeImage(width, height, filename);
+        if (resize.success) {
+            res.sendFile(resize.result, { root: path.join('./assets', 'thumbnails') });
+        } else {
+            res.send(resize.result)
+        }
+    }
+})
 
 export default images
