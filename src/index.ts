95df6adfd69f544a30aad7fd63b60d4a36d3a495
April <aprilnicole013@gmail.com>
2021-10-14 22:33:12 -0400

    Link help API route to main index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import images from './routes/images';
-import imageValidator from './middleware/imageValidator'
+import imageValidator from './middleware/imageValidator';
+import help from './routes/help'
 import express from 'express';
 
 const app = express();
 const port = 3000;
 
 app.use('/api/images', imageValidator, images);
+
+app.use('/api*', help);
 
 app.listen(port, () => {
     console.log(`server started at localhost:${port}`);
---
+++ src/routes/help.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+
+const help = express.Router();
+
+help.get('/', () => {})
+
+export default help;
